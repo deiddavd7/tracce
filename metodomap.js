4540ea6d771f149aa6fb52898af250d1c545a65f
Vincenzo Deidda <deiddavincenzo07@gmail.com>
2024-06-23 11:56:23 +0200

    map

---
+++ metodomap.js
@@ -0,0 +1,8 @@
+// Array iniziale di nomi
+let nomi = ["Daniel", "Vincenzo", "Antonio", "Alberto"];
+
+// Utilizzo del metodo map per aggiungere il testo a ciascun nome
+let nomiModificati = nomi.map(nome => `${nome} - ho imparato il metodo map`);
+
+// Stampa l'array modificato
+console.log(nomiModificati);
